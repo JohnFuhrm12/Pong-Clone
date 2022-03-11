d9133c9ef8645dfe126e287989444b37ce84aa22
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2022-03-10 21:57:53 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ pygame.init()
 # Cursors
 C_WIDTH = 18
 C_HEIGHT = 100
-VEL = 5
+VEL = 10
 
 # Screen Variables
 WIDTH, HEIGHT = 800, 600
@@ -25,7 +25,6 @@ TITLE_FONT = pygame.font.SysFont('impact', 100)
 TITLE_TEXT = TITLE_FONT.render(
     ("PONG"), 1, WHITE)
 MULT_BUTTON = button.Button(200, 200, button.BUTTON_TEXT)
-
 
 # On-Screen Objects
 def draw_objects():
@@ -53,6 +52,10 @@ def main_menu():
 
 # Main multiplayer game loop
 def game_loop_mult():
+    # Ball Variables
+    ball_speed_x = -14
+    ball_speed_y = 14
+
     LEFT_X = 30
     LEFT_Y = 250
 
@@ -89,14 +92,27 @@ def game_loop_mult():
         if keys[pygame.K_k] and RIGHT_Y < 590 - C_HEIGHT:
             RIGHT_Y += VEL
 
-        BALL_X -= 3
+        BALL_X += ball_speed_x
+        BALL_Y += ball_speed_y
 
         left_cursor = pygame.draw.rect(WIN, WHITE, (LEFT_X, LEFT_Y, C_WIDTH, C_HEIGHT))
         right_cursor = pygame.draw.rect(WIN, WHITE, (RIGHT_X, RIGHT_Y, C_WIDTH, C_HEIGHT))
         ball = pygame.draw.circle(WIN, (WHITE), [BALL_X, BALL_Y], 15, 0)
 
         if left_cursor.colliderect(ball):
-            BALL_X += 3
+            ball_speed_x *= -1
+        if right_cursor.colliderect(ball):
+            ball_speed_x *= -1
+
+        if BALL_X < 10:
+            ball_speed_x *= -1
+        if BALL_X > 790:
+            ball_speed_x *= -1
+
+        if BALL_Y < 10:
+            ball_speed_y *= -1
+        if BALL_Y > 590:
+            ball_speed_y *= -1
 
         pygame.display.update()
 
